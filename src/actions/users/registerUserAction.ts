0f26bee3ec058e93e09178f1b0dac1c7e41b4e42
willys123 <willysimatupang137@gmail.com>
2023-12-22 15:28:10 +0700

    feat: add hashing password

--- src/actions/users/registerUserAction.ts
+++ src/actions/users/registerUserAction.ts
@@ -3,13 +3,14 @@
 // kalau ada langsung return email / username telah digunakan
 // kalau tidak ada yang menggunakan email/username tsb create ke database
 
+import { hashPassword } from "../../helper/bcrypt";
 import { createUser } from "../../repositories/users/createUser";
 import { findUsersByEmailAndUsername } from "../../repositories/users/findUsersByEmailAndUsername";
 import { Iuser } from "../../types/user.type";
 
 export const registerUserAction = async (data: Iuser) => {
   try {
-    const { email, username } = data;
+    const { email, username, password } = data;
     const users = await findUsersByEmailAndUsername(email, username);
 
     if (users.length) {
@@ -19,6 +20,8 @@ export const registerUserAction = async (data: Iuser) => {
       };
     }
 
+    const hashedPassword = await hashPassword(password);
+    data.password = hashedPassword;
     //create users di database
     await createUser(data);
 
