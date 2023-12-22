a34a7ede259b3b50c6c331e5d08697c0fb9ba24b
willys123 <136543857+willys123@users.noreply.github.com>
2023-12-22 15:59:39 +0700

    feat: add compare password bcrypt login (#4)

--- src/helper/bcrypt.ts
+++ src/helper/bcrypt.ts
@@ -4,3 +4,10 @@ export const hashPassword = async (password: string): Promise<string> => {
   const saltRounds = 10;
   return await bcrypt.hash(password, saltRounds);
 };
+
+export const comparePasswords = async (
+  candidatePassword: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  return await bcrypt.compare(candidatePassword, hashedPassword);
+};
