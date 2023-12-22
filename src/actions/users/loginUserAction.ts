1d972e12e9eddcd33030a44d415f9b628bbd8f12
willys123 <136543857+willys123@users.noreply.github.com>
2023-12-22 14:51:35 +0700

    feat: add login feature (#2)

---
+++ src/actions/users/loginUserAction.ts
@@ -0,0 +1,58 @@
+// LOGIC
+// pertama check dulu apakah variabel usernameOrEmail itu adalah email atau bukan
+// kalau email cari data email tsb didatabase
+// kalau username cari data username tsb di database
+// kalau misalnya datanya ga ada ? berarti langsung return "account not found"
+// kalau datanya ada baru kita cek password yang ada didatabase dengan password yang dimasukkan oleh user (req.body)
+// kalau misalnya password ga sama, maka return salah password
+// kalau password nya sama, maka return success beserta data usernya
+
+import { findUserByEmail } from "../../repositories/users/findUserByEmail";
+import { findUserByUsername } from "../../repositories/users/findUserByUsername";
+
+export const loginUserAction = async (
+  usernameOrEmail: string,
+  password: string
+) => {
+  try {
+    let user;
+
+    if (usernameOrEmail.includes("@")) {
+      // find user by email
+      user = await findUserByEmail(usernameOrEmail);
+    } else {
+      // find user by username
+      user = await findUserByUsername(usernameOrEmail);
+    }
+
+    if (!user) {
+      return {
+        status: 404,
+        message: "Account not found",
+      };
+    }
+
+    if (user.isDeleted) {
+      return {
+        status: 400,
+        message: "Account deleted",
+      };
+    }
+
+    if (user.password !== password) {
+      return {
+        status: 400,
+        message: "Invalid credentials",
+      };
+    }
+
+    return {
+      status: 200,
+      message: "Login success",
+      data: user,
+    };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
---
+++ src/repositories/users/findUserByEmail.ts
@@ -0,0 +1,15 @@
+import { prisma } from "../../helper/prisma";
+
+export const findUserByEmail = async (email: string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
